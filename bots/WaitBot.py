567852e653938e6d11ca4887596aad86db38a6ac
nikita.prikazchikov <nprikazchikov@mirantis.com>
2013-05-22 09:56:21 +0400

    Refactored wait bot for class call conditions

--- bots/WaitBot.py
+++ bots/WaitBot.py
@@ -1,9 +1,12 @@
 import logging
+import time
+
 from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.support.wait import WebDriverWait
-import time
+
 from ContextHolder import ContextHolder
 from bots.baseBot import BaseBot
+
 
 __author__ = 'nprikazchikov'
 
@@ -21,7 +24,7 @@ class WaitBot(BaseBot):
             timeout = self._timeout
         return WebDriverWait(ContextHolder.get_driver(), timeout)
 
-    def _wait_for1(self, condition, timeout=5):
+    def _wait_for(self, condition, timeout=5):
         try:
             result = self._get_driver_wait(timeout).until(condition)
         except TimeoutException:
@@ -32,108 +35,78 @@ class WaitBot(BaseBot):
             result = None
         return result
 
-    def _wait_for(self, condition, by=None, value=None, parent=None,
-                  timeout=5):
-        result = None
-
-        arguments = {}
-
-        if not by is None:
-            arguments["by"] = by
-        if not value is None:
-            arguments["value"] = value
-
-        arguments["parent"] = parent
-
-        try:
-            result = self._get_driver_wait(timeout).until(
-                wait_for(condition, arguments)
-            )
-        except TimeoutException:
-            result = None
-            logger.debug("Waiting for event failed. Timeout exception")
-        except WebDriverException, e:
-            logger.error(repr(e))
-            result = None
-        return result
-
-    def _wait_for_element(self, condition, element=None, timeout=5):
-        result = None
-
-        arguments = {"element": element}
-
-        try:
-            result = self._get_driver_wait(timeout).until(
-                wait_for(condition, arguments)
-            )
-        except TimeoutException:
-            result = None
-            logger.debug("Waiting for event failed. Timeout exception")
-        except WebDriverException, e:
-            logger.error(repr(e))
-            result = None
-        return result
-
     def wait_for_stop_moving(self, by, value, parent=None):
-        return self._wait_for1(
+        return self._wait_for(
             stop_moving(self.find_element(by, value, parent))
         )
 
     def wait_for_web_element(self, by, value, parent=None):
         return self._wait_for(
-            condition=lambda by, value, parent:
-            self.find_element(by, value, parent),
-            by=by,
-            value=value,
-            parent=parent
+            wait_for(
+                condition=lambda by, value, parent:
+                self.find_element(by, value, parent),
+                by=by,
+                value=value,
+                parent=parent
+            )
         )
 
     def wait_for_disappears(self, by, value, parent=None):
         return self._wait_for(
-            condition=lambda by, value, parent:
-            not self.is_displayed(by, value, parent),
-            by=by,
-            value=value,
-            parent=parent
+            wait_for(
+                condition=lambda by, value, parent:
+                not self.is_displayed(by, value, parent),
+                by=by,
+                value=value,
+                parent=parent
+            )
         )
 
     def wait_for_web_element_disappears(self, web_element):
-        return self._wait_for_element(
-            condition=lambda element:
-            not self.is_element_displayed(element),
-            element=web_element
+        return self._wait_for(
+            wait_for(
+                condition=lambda element:
+                not self.is_element_displayed(element),
+                element=web_element
+            )
         )
 
     def wait_for_collapse(self, by, value, parent=None):
         return self._wait_for(
-            condition=lambda by, value, parent:
-            not self.is_element_exists(by, value, parent),
-            by=by,
-            value=value,
-            parent=parent
+            wait_for(
+                condition=lambda by, value, parent:
+                not self.is_element_exists(by, value, parent),
+                by=by,
+                value=value,
+                parent=parent
+            )
         )
 
     def wait_for_displays(self, by, value, parent=None):
         return self._wait_for(
-            condition=lambda by, value, parent:
-            self.is_displayed(by, value, parent),
-            by=by,
-            value=value,
-            parent=parent
+            wait_for(
+                condition=lambda by, value, parent:
+                self.is_displayed(by, value, parent),
+                by=by,
+                value=value,
+                parent=parent
+            )
         )
 
     def wait_for_web_element_displays(self, web_element):
-        return self._wait_for_element(
-            condition=lambda element:
-            self.is_element_displayed(element),
-            element=web_element
+        return self._wait_for(
+            wait_for(
+                condition=lambda element:
+                self.is_element_displayed(element),
+                element=web_element
+            )
         )
 
 
 class wait_for(object):
-    def __init__(self, condition, arguments):
+    def __init__(self, condition, *args, **kwargs):
         self.condition = condition
-        self.args = arguments
+        self.args = kwargs
 
     def __call__(self, driver):
         return self.condition(**self.args)
