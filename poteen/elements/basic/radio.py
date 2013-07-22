ff86953d26d3688694dd57f6863e4ad6e0070b29
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-07-22 17:11:33 +0400

    Revert Radio button get value function

--- poteen/elements/basic/radio.py
+++ poteen/elements/basic/radio.py
@@ -1,7 +1,5 @@
-from selenium.webdriver.common.by import By
 from ..baseElement import BaseElement
 from ...contextHolder import ContextHolder
-from engine.poteen.bots.actionBot import ActionBot
 from ...log.result import Result
 from ...log.resultList import ResultList
 
@@ -34,9 +32,8 @@ class Radio(BaseElement):
         return value
 
     def get_value(self):
-        element = ActionBot().find_element(by=By.TAG_NAME, value='input', parent=self.get_element())
         return self.VALUE_ON \
-            if element.is_selected() \
+            if self.get_element().is_selected() \
             else self.VALUE_OFF
 
     def set_value(self, value):
